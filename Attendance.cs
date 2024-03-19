cc8824b761e6f41bb5549d67bbe8077b0d91b0aa
yashwanthgh <yugnasura@gmail.com>
2024-03-19 09:44:56 +0530

    Added: Attendance functionality

---
+++ Attendance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageRemastered
+{
+    public class Attendance
+    {
+
+        Random random = new Random();   
+        public void PresentAbsent()
+        {
+            int attendane = random.Next(0, 2);
+            if(attendane == 0)
+            {
+                Console.WriteLine("Employee Absent");
+            } else
+            {
+                Console.WriteLine("Employee Present");
+            }
+        }
+    }
+}
