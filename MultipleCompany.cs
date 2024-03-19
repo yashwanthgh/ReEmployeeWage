a9f14b5e31a0f35c0cbd51a9b61ee8b62256c719
yashwanthgh <yugnasura@gmail.com>
2024-03-19 12:03:16 +0530

    Added: Functionality to Calculate wage for differnet company

---
+++ MultipleCompany.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageRemastered
+{
+    public class MultipleCompany
+    {
+        public int ComputeEmployeeWage(string companyName, int totalWorkingdays, int totalWorkingHours, int hourlyPay)
+        {
+            TotalWageInMonth totalWageInMonth = new TotalWageInMonth(companyName, totalWorkingdays,totalWorkingHours,hourlyPay);
+            return totalWageInMonth.CalculateTotalWage();
+        }
+    }
+}
