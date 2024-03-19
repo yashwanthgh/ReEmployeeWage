2a27083d84bb39bbf7623f6704b52e6706ae9b4e
yashwanthgh <yugnasura@gmail.com>
2024-03-19 12:47:46 +0530

    Added: Functionality to store multiple companies

---
+++ CompanyLists.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageRemastered
+{ 
+    internal class CompanyLists
+    {
+        static MultipleCompany[] companyLists = new MultipleCompany[5];
+        static int index = 0;   
+
+        public void AddCompanies(MultipleCompany company)
+        {
+            companyLists[index++] = company;
+        }
+    }
+}
